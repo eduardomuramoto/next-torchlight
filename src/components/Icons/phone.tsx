7647c70cca0985e4675999b61bde23d341c1a849
Eduardo Muramoto <dudmuramoto@gmail.com>
2024-09-21 19:44:28 +1000

    fix

---
+++ src/components/Icons/phone.tsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+import { SVGProps } from "react"
+const PhoneComponent = (props: SVGProps<SVGSVGElement>) => (
+  <svg width="26" height="32" viewBox="0 0 26 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+<g filter="url(#filter0_d_507_979)">
+<path d="M9.08292 3.1849C9.65178 3.01339 10.2622 3.04189 10.8126 3.26564C11.363 3.4894 11.8202 3.89492 12.108 4.4147L12.2042 4.6084L13.0648 6.5207C13.3255 7.10124 13.41 7.74552 13.3079 8.37365C13.2057 9.00177 12.9214 9.58608 12.4902 10.0541L12.3173 10.2257L10.9614 11.4906C10.717 11.722 10.9003 12.6177 11.7843 14.1491C12.5799 15.5271 13.2273 16.1706 13.5458 16.2057H13.6017L13.6706 16.1927L16.3356 15.3776C16.6938 15.2678 17.076 15.2634 17.4366 15.3651C17.7972 15.4668 18.1208 15.6701 18.3688 15.9509L18.4871 16.1004L20.2512 18.5444C20.597 19.0234 20.7692 19.6059 20.7397 20.196C20.7102 20.786 20.4806 21.3484 20.0887 21.7905L19.9301 21.9543L19.2255 22.6225C18.5928 23.2217 17.8013 23.6266 16.9452 23.7891C16.0891 23.9515 15.2043 23.8647 14.396 23.539C11.8805 22.525 9.59512 20.2084 7.51902 16.6126C5.43902 13.0077 4.57452 9.8656 4.96062 7.1746C5.07746 6.36115 5.41243 5.59453 5.92995 4.95615C6.44747 4.31777 7.12823 3.83147 7.89992 3.5489L8.15082 3.4657L9.08292 3.1849Z" fill="#F6FAF9"/>
+</g>
+<defs>
+<filter id="filter0_d_507_979" x="-4" y="0.5" width="34" height="34" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_507_979"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_507_979" result="shape"/>
+</filter>
+</defs>
+</svg>
+
+
+)
+export default PhoneComponent
