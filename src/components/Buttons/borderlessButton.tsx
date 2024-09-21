26fcb52305133aea29e8c6abed3e095ae375cd79
Eduardo Muramoto <dudmuramoto@gmail.com>
2024-09-21 19:44:28 +1000

    fix

--- src/components/Buttons/borderlessButton.tsx
+++ src/components/Buttons/borderlessButton.tsx
@@ -3,7 +3,10 @@ import SimpleArrowComponent from "../Icons/simpleArrow"
 export default function BorderlessButton({label, afterIcon }:{ label:string, afterIcon?:string}) {
   return (
     <button className="group flex items-center justify-center transition-all duration-150 ease-in-out font-semibold hover:font-bold  text-background py-2 pl-4 rounded">
-        {label}
+        <span className="relative">
+          {label}
+        <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-background transition-all group-hover:w-full"></span>
+        </span>
       {afterIcon?<SimpleArrowComponent className="transition-all duration-150 ease-in-out group-hover:translate-x-0.5 ml-2"/>:""}
     </button>
   )}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,49 @@
+"use client";
 import Image from "next/image";
 import logo from "@/../public/images/Logo_Horizontal.png"
 import OutlineButton from "./Buttons/outlineButton";
+import PhoneOutlineComponent from "./Icons/phone-outline";
+import PhoneComponent from "./Icons/phone";
+import { gql, useQuery } from '@apollo/client';
+
+
+
+
+
+// export async function getStaticProps() {
+//   const res = await fetch('http://localhost:1337/api/menus/1?nested&populate=*',{
+//     method: "GET",
+//     headers: {
+//       "X-Auth-Token": "ef72570ff371408f9668e414353b7b2e",
+//       "Content-Type": "application/json"
+//     }});
+//   const mainMenu = await res.json();
+//   console.log(mainMenu);
+//   return { props: { mainMenu } };
+// }
+
+// getStaticProps();
+// console.log(props);
 
 export default function Header() {
+  const GET_MENU = gql`
+  query GetMenu {
+    menus {
+      id
+    }
+  }
+`;
+  const { data, loading, error } = useQuery(GET_MENU, { 
+    variables: { id: 1 }
+  });
+  console.log(data);
   return (
     <header className="bg-foreground text-center text-surface/75 text-white/75 lg:text-left sticky top-0 z-10">
       <div className="px-20">
         <ul className="flex justify-between">
-          <li>000</li>
-          <li>1800 End Abuse</li>
-          <li>1800 363 228</li>
+          <li className="flex items-center py-1"><PhoneOutlineComponent/>000</li>
+          <li className="flex items-center py-1"><PhoneComponent/>1800 End Abuse</li>
+          <li className="flex items-center py-1"><PhoneComponent/>1800 363 228</li>
         </ul>
         </div>
       <div className="flex justify-between items-center bg-white py-2 px-20 text-center text-surface/75 text-white/75 lg:text-left">
