bacc2a0df001892e47a7c617e43d7ffdf7bec274
Eduardo Muramoto <dudmuramoto@gmail.com>
2024-09-23 15:30:00 +1000

    footer variables

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -91,58 +91,38 @@ export default function Footer() {
           <div>
             <h6
               className="mb-4 flex justify-center font-teko font-semibold uppercase md:justify-start">
-              Products
+             About Us
             </h6>
             <p className="mb-4">
-              <a href="#!">Angular</a>
+              <a href="#!">Contact</a>
             </p>
             <p className="mb-4">
-              <a href="#!">React</a>
-            </p>
-            <p className="mb-4">
-              <a href="#!">Vue</a>
+              <a href="#!">Contact</a>
             </p>
             <p>
-              <a href="#!">Laravel</a>
+              <a href="#!">Contact</a>
             </p>
           </div>
           <div>
             <h6
               className="mb-4 flex justify-center font-teko font-semibold uppercase md:justify-start">
-              Useful links
+              Get Support
             </h6>
             <p className="mb-4">
-              <a href="#!">Pricing</a>
+              <a href="#!">Courses</a>
             </p>
             <p className="mb-4">
-              <a href="#!">Settings</a>
+              <a href="#!">Services</a>
             </p>
             <p className="mb-4">
-              <a href="#!">Orders</a>
-            </p>
-            <p>
-              <a href="#!">Help</a>
+              <a href="#!">Enquiry</a>
             </p>
           </div>
           <div>
             <h6
               className="mb-4 flex justify-center font-teko font-semibold uppercase md:justify-start">
-              Contact
+              Contact Us
             </h6>
-            <p className="mb-4 flex items-center justify-center md:justify-start">
-              <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor">
-                  <path
-                    d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
-                  <path
-                    d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
-                </svg>
-              </span>
-              New York, NY 10012, US
-            </p>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
                 <svg
@@ -155,7 +135,7 @@ export default function Footer() {
                     d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                 </svg>
               </span>
-              info@example.com
+              hq@torchlight.org.au
             </p>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
@@ -169,7 +149,7 @@ export default function Footer() {
                     clip-rule="evenodd" />
                 </svg>
               </span>
-              + 01 234 567 88
+              1800 END ABUSE
             </p>
             <p className="flex items-center justify-center md:justify-start">
               <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
@@ -183,7 +163,7 @@ export default function Footer() {
                     clip-rule="evenodd" />
                 </svg>
               </span>
-              + 01 234 567 89
+              1800 363 228
             </p>
           </div>
           <div className="">
