3929a6d478bc2364093a158f476bd565ec284599
Eduardo Muramoto <dudmuramoto@gmail.com>
2024-10-30 16:50:49 +1100

    volunteer form function

--- src/app/get-involved/volunteer/page.tsx
+++ src/app/get-involved/volunteer/page.tsx
@@ -1,8 +1,6 @@
-import EnquirySection from "@/components/ ContactSection/enquiry";
-import OverlapSection from "@/components/Sections/OverlapSection";
 import JumboHeader from "@/components/Sections/pageJumboHeader";
-import volunteerPhoto from "@images/Volunteer.jpg"
-import donatePhoto from "@images/donate.webp"
+import getInvolvedPhoto from "@images/get_involved.jpg"
+import VolunteerFormSection from "@/components/ ContactSection/volunteerForm";
 
 
 
@@ -14,37 +12,13 @@ export default function VolunteerPage() {
       <main className="flex flex-col row-start-2 items-center sm:items-start">
         <JumboHeader
           alignTop={false}
-          subTitle="Get Involved"
-          title="VOLUNTEER"
-          imgUrl={volunteerPhoto}
-          paragraphs={["Join us in making a difference in the lives of survivors of domestic violence. Volunteer with our DV service and help provide support, empowerment, and resources to those in need.",
-            "Together, we can create a safe and nurturing environment for survivors to heal and rebuild their lives. Whether you have a few hours to spare or are looking for a long-term commitment, your time and dedication can make a significant impact.",
-            "Join our team of compassionate volunteers today and be a part of the solution.",
-            "Together, let's break the cycle of domestic violence and create a brighter future for all."
-          ]}
-          button={{ label: "Become a volunteer", url: "/get-involved/volunteer" }}
-        />
-        {/* <OverlapSection
-          style="red"
-          subTitle="Donate" 
-          title="Support us financially on our mission to end abuse." 
-          imgUrl={donatePhoto}
-          paragraphs={[
-            "Shine a light on hope — help us break the cycle of domestic violence. Your donation to the Torchlight Foundation empowers survivors and ignites a future of safety and healing."
-                      ]} 
-        /> */}
-        <OverlapSection
-          reverse={true}
-          style="red"
-          subTitle="Donate"
-          title="Support us financially on our mission to end abuse."
-          imgUrl={donatePhoto}
-          paragraphs={[
-            "Shine a light on hope — help us break the cycle of domestic violence. Your donation to the Torchlight Foundation empowers survivors and ignites a future of safety and healing."
+          title="Become a volunteer"
+          imgUrl={getInvolvedPhoto}
+          paragraphs={["Volunteers are essential to the Torchlight Foundation, driving meaningful change in communities by supporting survivors of domestic violence, advocating for change, and promoting awareness. We welcome individuals from all walks of life who are passionate about making a difference and creating safer, more compassionate communities."
           ]}
         />
 
-        <EnquirySection />
+        <VolunteerFormSection />
       </main>
 
     </div>
---
+++ src/components/ ContactSection/volunteerForm.tsx
@@ -0,0 +1,155 @@
+"use client"
+import React, { useState } from "react";
+import BlueButton from "../Buttons/blueButton";
+
+// Define the shape of the form data
+interface FormData {
+  name: string;
+  email: string;
+  phone: string;
+  organization: string;
+  message: string;
+}
+
+// Define the shape of the status message
+interface StatusMessage {
+  message: string;
+  type: "success" | "error" | ""; // Possible types for the status message
+}
+
+export default function VolunteerFormSection() {
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+    phone: "",
+    organization: "",
+    message: "",
+  });
+
+  const [statusMessage, setStatusMessage] = useState<StatusMessage>({
+    message: "",
+    type: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault();
+    e.stopPropagation();
+    setStatusMessage({ message: "Sending Email...", type: "" }); // Reset message before submission
+
+    try {
+      const response = await fetch("/api/volunteerEmail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to send email");
+      }
+
+      setStatusMessage({
+        message: "Email sent successfully!",
+        type: "success",
+      });
+      setFormData({
+        name: "",
+        email: "",
+        phone: "",
+        organization: "",
+        message: "",
+      }); // Clear form fields
+    } catch (error) {
+      console.error("Error sending email:", error);
+      setStatusMessage({
+        message: "Error sending email. Please try again later.",
+        type: "error",
+      });
+    }
+  };
+
+  return (
+    <section className="w-full bg-lightBackground flex gap-20 p-16 lg:px-20" id="enquiry">
+      <div className="w-full">
+        <h3 className="font-teko text-6xl uppercase font-semibold pb-6 text-background">Get Involved!</h3>
+        <p className="font-teko text-6xl uppercase font-semibold pb-6 text-background">Make a difference.</p>
+        <form onSubmit={handleSubmit} className="flex flex-wrap lg:flex-nowrap lg:gap-20 w-full">
+          <div className="w-full lg:w-1/3">
+            <div className="pb-2">
+              <label className="text-background font-semibold" htmlFor="name">Your Name *</label>
+              <input
+                id="name"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-foreground focus:outline-none focus:ring"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="pb-2">
+              <label className="text-background font-semibold" htmlFor="email">Email *</label>
+              <input
+                id="email"
+                type="email"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-foreground focus:outline-none focus:ring"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="pb-2">
+              <label className="text-background font-semibold" htmlFor="phone">Phone *</label>
+              <input
+                id="phone"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-foreground focus:outline-none focus:ring"
+                value={formData.phone}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="pb-2">
+              <label className="text-background font-semibold" htmlFor="organization">Organization</label>
+              <input
+                id="organization"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-foreground focus:outline-none focus:ring"
+                value={formData.organization}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="w-full lg:w-2/3 h-full ">
+            <div className="pb-6">
+              <label className="text-background font-semibold" htmlFor="message">Message *</label>
+              <textarea
+                id="message"
+                rows={8}
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-foreground focus:outline-none focus:ring"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <BlueButton className="ml-auto" label="Let's connect" afterIcon="yes" isSubmit={true} />
+          </div>
+        </form>
+        {/* Status Message */}
+        {statusMessage.message && (
+          <div className={`mt-4 text-lg ${statusMessage.type === "success" ? "text-green-600" : "text-red-600"}`}>
+            {statusMessage.message}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+}
