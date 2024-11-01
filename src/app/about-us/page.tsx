9406d74850d8222e515b1931a4e3f8ef30a89430
Eduardo Muramoto <dudmuramoto@gmail.com>
2024-11-01 16:09:50 +1100

    ceo section

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,4 +1,5 @@
 import EnquirySection from "@/components/ ContactSection/enquiry";
+import CeoSection from "@/components/Sections/ceoSection";
 import IconsSection from "@/components/Sections/iconsSection";
 import JumboHeader from "@/components/Sections/pageJumboHeader";
 import SubNavBar from "@/components/Sections/pageSubNavBar";
@@ -9,54 +10,62 @@ import visionPhoto from "@images/our_vision.jpg"
 
 
 export default function GetSupportPage() {
- 
+
   return (
     <div className="">
       <main className="flex flex-col row-start-2 items-center sm:items-start">
-        <JumboHeader 
+        <JumboHeader
           alignTop={false}
-          subTitle="About Us" 
-          title="Shining light for a brighter future" 
-          imgUrl={jumboPhoto} 
+          subTitle="About Us"
+          title="Shining light for a brighter future"
+          imgUrl={jumboPhoto}
           paragraphs={["Torchlight Foundation is committed to driving positive change against domestic violence by challenging the status quo and envisioning a safer future for all. We foster a culture of respect and compassion through education, advocacy, and support. Together, we strive to create a more united and compassionate society."]}
         />
-          <SubNavBar Links={[
-          {url:"#CEO",label:"CEO & Founder"},
-          {url:"#what-we-do",label:"What We Do"},
-          {url:"#our-mission",label:"Our Mission"},
-          {url:"#our-vision",label:"Our Vision"},
-        ]}/>
+        <SubNavBar Links={[
+          { url: "#CEO", label: "CEO & Founder" },
+          { url: "#what-we-do", label: "What We Do" },
+          { url: "#our-mission", label: "Our Mission" },
+          { url: "#our-vision", label: "Our Vision" },
+        ]} />
+        <CeoSection
+          title="CEO & Founder"
+          subTitle="Shine bright, and know that you are never alone."
+          paragraphs={[
+            "Our journey has been inspired by the resilience of survivors and your unwavering support. It's your commitment that allows us to work towards a future where abuse is preventable, survivors are met with compassion, and communities are empowered to drive transformation. I invite you to join us in making a lasting difference for those who need it most. Thank you for being a part of our Torchlight family.",
+            'In my own life, I experienced the harsh reality of an abusive marriage, which has shaped my mission today: Saving Survivors. Sharing my story of domestic violence has given me a deep understanding of the struggles many face. "If it could happen to me, it could happen to any of us, " is a truth that echoes throughout my journey. Making the decision to break free wasn’t easy, but it led me to launch the Torchlight Foundation. We stand with every survivor, supporting you every step of the way.'
+          ]}
+        />
         <IconsSection
           idName="what-we-do"
           title="What We Do"
           blueTitle={true}
           paragraphs={["we are dedicated to inspiring and empowering families and communities. By fostering advocacy and promoting positive changes in societal attitudes, culture, and policies, we strive to break the cycle of abuse and create a future where everyone can thrive in a safe and supportive environment."]}
         />
-        <JumboHeader 
+        <JumboHeader
           idName="our-mission"
           bgWhite={true}
           blueTitle={true}
           alignTop={false}
-          title="Our Mission" 
-          imgUrl={missionPhoto} 
+          title="Our Mission"
+          imgUrl={missionPhoto}
           paragraphs={["At Torchlight Foundation, we are committed to cultivating a culture of respect and compassion. We work to foster safe, stable, and caring relationships for both adults and children, with the ultimate goal of eradicating all forms of abuse, including domestic violence, child sexual abuse, discrimination, and bullying.",
             "By empowering individuals and communities with knowledge and inspiration, we aim to drive transformative shifts in societal attitudes, culture, and policies. Through greater awareness and advocacy, we envision a future where everyone feels safe, valued, and free from the impacts of abuse. Together, we can break the cycle and build a more compassionate society."
           ]}
         />
-        <JumboHeader 
+        <JumboHeader
           idName="our-vision"
           blueTitle={true}
           alignTop={false}
           reverse={true}
-          title="Our Vision" 
-          imgUrl={visionPhoto} 
+          title="Our Vision"
+          imgUrl={visionPhoto}
           paragraphs={["We envision a world where every individual, regardless of age or background, lives free from violence, discrimination, and fear. A society built on compassion, respect, and understanding, where families and communities can thrive in environments that nurture their well-being.",
             "Through education, advocacy, and transformative initiatives, we strive to break the cycle of abuse. Our vision is to create a future where everyone has the opportunity to flourish in a safe, supportive world—one where empathy and equality are the foundation of lasting change for generations to come."
           ]}
         />
-        <EnquirySection/>
+        <EnquirySection />
       </main>
-      
+
     </div>
   );
 }
--- src/components/Sections/ceoSection.tsx
+++ src/components/Sections/ceoSection.tsx
@@ -1,41 +1,47 @@
 import Image, { StaticImageData } from "next/image"
-import photo from "@images/donate.webp"
-import OrangeButton from "../Buttons/orangeButton";
+import photo from "@images/Lisa.png"
+import signature from "@images/LisaCotton.png"
 
-export default function CeoSection({title, subTitle, paragraphs, imgUrl, style="orange", reverse=false, smallPicture=false }:{title:string, subTitle:string, paragraphs:string[], imgUrl?:StaticImageData, style?:string, reverse?:boolean, smallPicture?:boolean}) {
-  const bgColor = style==="orange"? "bg-lightForeground": "bg-redBackground";
-  const titleColor = style==="orange"? "text-foreground": "text-background";
-  const reverseCols = reverse? "flex-row-reverse ":"";
+export default function CeoSection({ title, subTitle, paragraphs, imgUrl, style = "blue" }: { title: string, subTitle: string, paragraphs: string[], imgUrl?: StaticImageData, style?: string }) {
+  const bgColor = style === "orange" ? "bg-lightForeground" : "bg-lightForeground";
+  const titleColor = style === "orange" ? "text-foreground" : "text-background";
 
 
   return (
-    <div className={ reverseCols + "bg-white w-full flex-wrap lg:flex-nowrap flex lg:gap-20 p-16 lg:px-36 items-end"}>
-      <div className={(reverse? "lg:pr-8":"lg:pl-8")+" lg:pb-8 w-full "+(smallPicture?"lg:w-1/3":"lg:w-1/2")+" h-1/3 z-10"}>
-      <Image
-                  src={imgUrl?imgUrl:photo}
-                  alt="Get Support photo"
-                  className="lazyload rounded-t-lg lg:rounded-lg overflow-hidden"
-                  width={1200}
-                />
-      </div>
-      <div className={ bgColor + "  w-full lg:w-[80%] rounded-b-lg lg:absolute lg:rounded-lg"}>
-        <div className={(reverse? "lg:w-1/2 lg:mr":"lg:ml")+"-[50%] p-8"}>
-          {/* <p className="text-background text-2xl pb-6 font-semibold">{subTitle}</p> */}
-          <h1 className={titleColor +" font-teko text-4xl uppercase font-semibold pb-6"}>{title}</h1>
-          {paragraphs? paragraphs.map((paragraph) => {
-            return(<p className="text-gray-800 pb-4" key={paragraph}>{paragraph}</p>)
+    <div className={"bg-white w-full flex flex-col p-16 lg:px-36"}>
+      <div className="flex items-center flex-col relative">
+        <div className="absolute bottom-0 right-0 h-96 w-auto">
+          <Image
+            src={imgUrl ? imgUrl : photo}
+            alt="Get Support photo"
+            className="lazyload"
+            width={300}
+          />
+        </div>
+        <h2 className={titleColor + " font-teko text-4xl uppercase font-semibold pb-6"}>{title}</h2>
+        <div className={bgColor + "  w-[80%] rounded-lg p-12 mr-40 pr-36"}>
+          <h3 className="text-foreground font-teko uppercase text-6xl pb-6 font-semibold"><span className="pr-2">"</span>{subTitle}</h3>
+          {paragraphs ? paragraphs.map((paragraph) => {
+            return (<p className="text-gray-800 pb-4" key={paragraph}>{paragraph}</p>)
           })
-          :
-          <>
-          <p className="text-gray-800 pb-4">At Torchlight Foundation, our mission is to provide education and resources to support mental health and wellness in our community.</p>
-          <p className="text-gray-800 pb-4">We offer a variety of programmes and services to support mental health, including counselling, support groups, and educational workshops.</p>
-          <p className="text-gray-800 pb-4">Over the past year, we have helped over 1,000 individuals in our community improve their mental health and well-being.</p>
-          </>
+            :
+            <>
+              <p className="text-gray-800 pb-4">At Torchlight Foundation, our mission is to provide education and resources to support mental health and wellness in our community.</p>
+              <p className="text-gray-800 pb-4">We offer a variety of programmes and services to support mental health, including counselling, support groups, and educational workshops.</p>
+              <p className="text-gray-800 pb-4">Over the past year, we have helped over 1,000 individuals in our community improve their mental health and well-being.</p>
+            </>
           }
-          <a href="https://www.torchlight.org.au/donation" target="_blank">
-          <OrangeButton label="Donate"/>
-          </a>
+          <div className="justify-self-end">
+            <Image
+              src={signature}
+              alt="Lisa Cotton Signature"
+              className="lazyload"
+              width={120}
+            />
+            <p className="text-gray-800 text-xs">CEO & Founder</p>
+          </div>
         </div>
       </div>
     </div>
-  )}
+  )
+}
