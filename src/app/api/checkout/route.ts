d3e60999c6cc89affc0b901ee0e4bde4302c139b
Eduardo Muramoto <dudmuramoto@gmail.com>
2024-11-04 16:03:42 +1100

    fix route

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,5 +1,4 @@
-
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 
 interface Product {
   name: string;
@@ -11,32 +10,24 @@ interface CheckoutSessionResponse {
   sessionId: string;
 }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === 'POST') {
-    const { products }: { products: Product[] } = req.body; // Type the request body
+export async function POST(req: Request) {
+  const { products }: { products: Product[] } = await req.json(); // Get the JSON body
 
-    try {
-      // Call Strapi backend to create a checkout session
-      const response = await fetch(`${process.env.CMS_URL}/checkout`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ products }),
-      });
+  try {
+    // Call Strapi backend to create a checkout session
+    const response = await fetch(`${process.env.CMS_URL}/checkout`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ products }),
+    });
 
-      const { id }: { id: string } = await response.json(); // Expect session ID from Strapi
+    const { id }: { id: string } = await response.json(); // Expect session ID from Strapi
 
-      const data: CheckoutSessionResponse = { sessionId: id }; // Define the response type
-      res.status(200).json(data);
-    } catch (error) {
-      res.status(500).json({ error: 'Error creating checkout session' });
-    }
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    const data: CheckoutSessionResponse = { sessionId: id }; // Define the response type
+    return NextResponse.json(data); // Use NextResponse to return JSON
+  } catch (error) {
+    return NextResponse.json({ error: 'Error creating checkout session' }, { status: 500 });
   }
 }
