4e4f613bac29897467ebff7f6ca1c0d456149325
Eduardo Muramoto <dudmuramoto@gmail.com>
2024-11-12 16:45:43 +1100

    fix eslint errors

--- src/app/get-involved/donate/success/page.tsx
+++ src/app/get-involved/donate/success/page.tsx
@@ -1,51 +1,48 @@
-"use client"
-import { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { loadStripe } from "@stripe/stripe-js";
+// "use client"
+// import { useEffect, useState } from "react";
+// import { useSearchParams } from "next/navigation";
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string);
 
-const DonationSuccessPage = () => {
-  const router = useRouter();
-  const session_id = useSearchParams().get("session_id");
-  const [session, setSession] = useState<any | null>(null);
-  const [loading, setLoading] = useState(true);
+// const DonationSuccessPage = () => {
+//   const session_id = useSearchParams().get("session_id");
+//   const [session, setSession] = useState<string | null>(null);
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    if (!session_id) return;
+//   useEffect(() => {
+//     if (!session_id) return;
 
-    const fetchSession = async () => {
-      try {
-        const response = await fetch(`/api/donation-checkout-session/${session_id}`);
-        if (!response.ok) {
-          throw new Error("Failed to fetch session data");
-        }
-        const data = await response.json();
-        setSession(data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching session:", error);
-        setLoading(false);
-      }
-    };
+//     const fetchSession = async () => {
+//       try {
+//         const response = await fetch(`/api/donation-checkout-session/${session_id}`);
+//         if (!response.ok) {
+//           throw new Error("Failed to fetch session data");
+//         }
+//         const data = await response.json();
+//         setSession(data);
+//         setLoading(false);
+//       } catch (error) {
+//         console.error("Error fetching session:", error);
+//         setLoading(false);
+//       }
+//     };
 
-    fetchSession();
-  }, [session_id]);
+//     fetchSession();
+//   }, [session_id]);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+//   if (loading) {
+//     return <p>Loading...</p>;
+//   }
 
-  if (!session) {
-    return <p>Error: Could not retrieve session data.</p>;
-  }
-  return (
-    <div className="w-full bg-lightBackground flex flex-col items-center justify-center p-16 px-6 lg:px-20">
-      <h1 className="font-teko text-6xl uppercase font-semibold pb-6 text-background">Thank You, {session.customer_details.name}, for Your Donation!</h1>
-      <p className="font-teko text-4xl pb-6 text-background">Your donation of ${session.amount_total / 100} has been successfully processed.</p>
-      <p className="font-teko text-2xl pb-6 text-background">If you have any questions, please contact us.</p>
-    </div>
-  );
-};
+//   if (!session) {
+//     return <p>Error: Could not retrieve session data.</p>;
+//   }
+//   return (
+//     <div className="w-full bg-lightBackground flex flex-col items-center justify-center p-16 px-6 lg:px-20">
+//       <h1 className="font-teko text-6xl uppercase font-semibold pb-6 text-background">Thank You, {session.customer_details.name}, for Your Donation!</h1>
+//       <p className="font-teko text-4xl pb-6 text-background">Your donation of ${session.amount_total / 100} has been successfully processed.</p>
+//       <p className="font-teko text-2xl pb-6 text-background">If you have any questions, please contact us.</p>
+//     </div>
+//   );
+// };
 
-export default DonationSuccessPage;
+// export default DonationSuccessPage;
--- src/app/our-projects/page.tsx
+++ src/app/our-projects/page.tsx
@@ -1,8 +1,5 @@
-import ProjectCard from "@/components/Cards/projectCard";
 import JusticeRadioHeader from "@/components/Sections/justiceRadioSection";
 import ProjectsHeader from "@/components/Sections/projectsHeader";
-import CafePhoto from "@images/CAFE_Project.jpg"
-import DandelionPhoto from "@images/Dandelion_Project.jpg"
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
