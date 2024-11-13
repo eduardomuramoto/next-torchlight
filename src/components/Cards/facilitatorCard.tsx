9b4ec11ef0150d31d465303c0ebccc2c0f3d459f
Eduardo Muramoto <dudmuramoto@gmail.com>
2024-11-13 11:56:36 +1100

    disable dark mode

--- src/components/Cards/facilitatorCard.tsx
+++ src/components/Cards/facilitatorCard.tsx
@@ -10,7 +10,7 @@ export default function FacilitatorCard({ facilitator, description }: Facilitato
     <div className="flex flex-col items-center max-h-80 max-w-72 py-4 px-6 bg-lightForeground border-[3px] border-background text-background font-semibold rounded-md">
       <p>{facilitator}</p>
 
-      <p className="text-center text-gray-800 font-normal pb-2">{description}</p>
+      <p className="text-center text-gray-800 font-normal pb-2 pt-2">{description}</p>
     </div>
   )
 }
