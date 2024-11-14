6d8f1720f5c36002f867ac24bcb51e492bc7bd8f
Eduardo Muramoto <dudmuramoto@gmail.com>
2024-11-14 15:09:44 +1100

    new icons

--- src/components/Cards/iconCard.tsx
+++ src/components/Cards/iconCard.tsx
@@ -8,11 +8,11 @@ import { useState, useEffect } from "react";
 type IconCardProps = {
   icon: string,
   objective: string,
-  folder?:string,
+  folder?: string,
   isBold?: boolean
 }
 
-export default function IconCard({icon, objective, folder ="course", isBold=true}:IconCardProps) {
+export default function IconCard({ icon, objective, folder = "course", isBold = true }: IconCardProps) {
   const [imageData, setImageData] = useState<StaticImageData | null>(null);
   useEffect(() => {
     async function fetchImage() {
@@ -25,13 +25,14 @@ export default function IconCard({icon, objective, folder ="course", isBold=true
   return (
     <div className="flex flex-col items-center max-h-80 max-w-72 py-4 px-6 text-background font-semibold rounded-md">
       <Image
-                  src={imageData?imageData:iconImage}
-                  alt="icon image"
-                  className="lazyload w-[100px]"
-                  height={300}
-                  width={300}
-                />
-     
-      <p className={isBold?"font-bold text-background ":"text-gray-800 " + "text-center font-normal pb-2"}>{objective}</p>
+        src={imageData ? imageData : iconImage}
+        alt="icon image"
+        className="lazyload w-[100px]"
+        height={300}
+        width={300}
+      />
+
+      <p className={isBold ? "font-bold text-background " : "text-gray-800 " + "text-center font-normal pb-2"}>{objective}</p>
     </div>
-  )}
+  )
+}
