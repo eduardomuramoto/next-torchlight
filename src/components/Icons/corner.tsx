2b440df142b10e28653b088d71391b3586d0101e
Eduardo Muramoto <dudmuramoto@gmail.com>
2024-11-14 15:44:11 +1100

    remove shaddow

--- src/components/Icons/corner.tsx
+++ src/components/Icons/corner.tsx
@@ -1,24 +1,12 @@
 import * as React from "react"
 
-export default function CornerComponent({className, color: color}:{className: string, color:string}) {
-    return (
-  <svg width="185" height="111" viewBox="0 0 185 111" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
-<g filter="url(#filter0_d_507_1792)">
-<path className={color} d="M186.481 -2.00067V102.091C186.361 99.4702 186.121 96.705 185.641 94.4985C182.171 78.4369 177.961 74.4384 172.151 64.9093C158.041 41.766 120.551 25.2226 103.611 21.7059C80.0114 16.8113 12 5.00122 4 -2.00067C4 -2.00067 33.5758 -1.67459 103.611 -2.00188C154.478 -2.2396 158.5 -2.00157 186.481 -2.00067Z" fill="#FF4081"/>
-</g>
-<defs>
-<filter id="filter0_d_507_1792" x="0" y="-2.10742" width="190.481" height="112.199" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feOffset dy="4"/>
-<feGaussianBlur stdDeviation="2"/>
-<feComposite in2="hardAlpha" operator="out"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_507_1792"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_507_1792" result="shape"/>
-</filter>
-</defs>
-</svg>
+export default function CornerComponent({ className, color: color }: { className: string, color: string }) {
+  return (
+    <svg width="185" height="111" viewBox="0 0 185 111" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
+      <path className={color} d="M182.481 -2.00067V102.091C182.361 99.4702 182.121 96.7049 181.641 94.4985C178.171 78.4369 173.961 74.4384 168.151 64.9093C154.041 41.766 116.551 25.2226 99.6114 21.7059C76.0114 16.8113 8 5.00122 0 -2.00067C0 -2.00067 29.5758 -1.67459 99.6109 -2.00188C150.478 -2.2396 154.5 -2.00157 182.481 -2.00067Z" fill="#FF4081" />
+
+    </svg>
 
 
-    )}
+  )
+}
